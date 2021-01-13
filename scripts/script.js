060b8fb7453c9fbd4c0f1f0af67dbd5377844f36
GettsuGettsu <gettsuga.tenso@gmail.com>
2021-01-13 19:03:33 +0300

    Some improvements

--- scripts/script.js
+++ scripts/script.js
@@ -4,24 +4,26 @@ const bricks = brField.getElementsByTagName('td');
 const ball = document.getElementById('ball');
 const paddle = document.getElementById('paddle');
 const inpt = document.getElementById('input');
+
 let score = 0;
-let lifes = 3;
-var Ball = {
+let lifes = 300;
+
+let Ball = {
 	x: 947.5,
 	y: 926,
 	width: 25,
 	height: 25,
 	dx: 0,
 	dy: 0,
-	v: 5
+	v: 7
 }
-var Paddle = {
+let Paddle = {
 	x:886,
 	y:951,
 	height: 25,
 	width: 148
 }
-var bord = {
+let bord = {
 	y: 10,
 	left: 567.5,
 	right: 1352.5,
@@ -33,6 +35,11 @@ function collides(obj1, obj2) {
          obj1.x + obj1.width > obj2.x &&
          obj1.y < obj2.y + obj2.height &&
          obj1.y + obj1.height > obj2.y;
+}
+
+function dxRefl(k, dx) {
+	if (dx > 0) return dx = k;
+	else  return dx = -k;
 }
 
 document.addEventListener('keydown', function (e) {
@@ -74,27 +81,39 @@ function cycle() {
 		Ball.y = bord.y;
 		Ball.dy *= -1;
 	}
-	if (Ball.y >= bord.bottom - Ball.width - 1) {
+	if (Ball.y >= bord.bottom - Ball.width) {
 		if (lifes > 1) {
 			lifes--;
-			Ball.y = bord.bottom - Ball.width - 1;
+			Ball.y = bord.bottom - Ball.width;
 			Ball.dy *= -1;
 		}
 		else {
 			alert('Game over');
-			/*Ball.x = 947.5;
-			Ball.y = 926;
-			Ball.dx = 0;
-			Ball.dy = 0;
-			lifes= 3;*/
 			location.reload();
 		}
 	}
 	
 	if (collides(Ball, Paddle)) { 
+		if ((Ball.x + Ball.width > Paddle.x && Ball.x + Ball.width/2 <= Paddle.x + Paddle.width/5) ||
+		(Ball.x + Ball.width/2 >= Paddle.x + Paddle.width * 4/5 && Ball.x - Ball.width/2 < Paddle.x + Paddle.width)) {
+			Ball.dy *= -3 / Ball.dy;
+			Ball.dx = dxRefl(10, Ball.dx);
+			Ball.y = Paddle.y - Ball.height;
+		} else if (Ball.x + Ball.width/2 > Paddle.x + Paddle.width/5 && Ball.x + Ball.width/2 < Paddle.x + Paddle.width * 4/5) {
+			Ball.dy *= -5 / Ball.dy;
+			Ball.dx = dxRefl(5, Ball.dx);
+			Ball.y = Paddle.y - Ball.height;
+		} else if (Ball.y + Ball.height === Paddle.y && (Ball.x + Ball.width === Paddle.x || Ball.x  === Paddle.x + Paddle.width)) {
+			Ball.dy *= -1;
+			Ball.dx *= -1;
+		} else if (Ball.y + Ball.height > Paddle.y && (Ball.x + Ball.width <= Paddle.x || Ball.x >= Paddle.x + Paddle.width)) {
+			Ball.dx *= -1;
+		}
+	}
+	/*if (collides(Ball, Paddle)) { 
 		Ball.y = Paddle.y - Ball.height; 
 		Ball.dy *= -1;
-	}
+	}*/
 	
 	for (let i = 0; i < bricks.length; i++) {
 		let brick = bricks[i].getBoundingClientRect();
@@ -119,6 +138,6 @@ function cycle() {
 	
 	inpt.setAttribute('value', "lifes: " + lifes);
 }
-requestAnimationFrame(cycle());
+requestAnimationFrame(cycle);
 
 
